08ad0f6b1b6914fcd9317fc0d6283017e314b69b
Memopus <yassinesaq2007@gmail.com>
2022-11-29 19:34:23 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const container = document.querySelector(".container");
+const eraser = document.querySelector(".eraser");
+const rgbButton = document.querySelector(".rgb");
+const palette = document.querySelector("#palette");
+const input = document.querySelector("#squares");
+const label = document.querySelector("#squaresNumber");
+let refresh = false;
+input.addEventListener("change", () => {
+  refreshGrid();
+  createGrid();
+});
+
+function createGrid() {
+  for (let i = 0; i < input.value ** 2; i++) {
+    const div = document.createElement("div");
+    div.style.borderColor = "black";
+    div.style.width = `${container.offsetWidth / input.value}px`;
+    div.style.width = `${container.offsetHeight / input.value}px`;
+    div.classList.add("square");
+    container.appendChild(div);
+    changeColor(div);
+    eraser.addEventListener("click", () => {
+      div.style.backgroundColor = "transparent";
+    });
+    input.addEventListener("change", () => {
+      div.remove();
+    });
+  }
+
+  // rgbButton.addEventListener("click", () => {
+  //   rgbMode(div);
+  // });
+}
+
+function refreshGrid() {
+  refresh = true;
+  return;
+}
+
+function changeColor(div) {
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = palette.value;
+  });
+
+  rgbButton.addEventListener("click", () => {
+    rgbMode(div);
+  });
+
+  console.log(palette.value);
+}
+function rgbMode(div) {
+  div.addEventListener("mouseover", () => {
+    const r = Math.floor(Math.random() * 255) + 1;
+    const g = Math.floor(Math.random() * 255) + 1;
+    const b = Math.floor(Math.random() * 255) + 1;
+    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+  });
+}
